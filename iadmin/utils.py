7343a54ee4b14ca18b02a716436c954f5f783e14
saxix <saxix.rome@gmail.com>
2011-05-25 11:41:23 +0200

    added Iline parameter and kwargs handling

--- iadmin/utils.py
+++ iadmin/utils.py
@@ -1,14 +1,12 @@
-#
-from . import options
 
-__author__ = 'sax'
+from iadmin.options import ITabularInline
 
-
-def tabular_factory(model, fields=None, **kwargs):
+def tabular_factory(model, fields=None, Inline=ITabularInline, **kwargs):
     name = "%sInLine" % model.__class__.__name__
     form = "%sForm" % model.__class__.__name__
-    attrs = {'model': model, 'fields': fields }
+    attrs = {'model': model, 'fields': fields}
     if form in globals():
         attrs['form'] = form
-    Tab = type(name, (options .ITabularInline,), attrs)
+    attrs.update(kwargs)
+    Tab = type(name, (Inline,), attrs)
     return Tab
