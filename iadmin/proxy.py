dfef81849fbfee7a94b6c9c95f8555b732a9b89a
saxix <saxix.rome@gmail.com>
2011-06-13 16:56:39 +0200

    allow proxy to import I*

--- iadmin/proxy.py
+++ iadmin/proxy.py
@@ -1,3 +1,4 @@
 from django.contrib.admin import *
 from iadmin.sites import IAdminSite, site
 from iadmin.options import IModelAdmin as ModelAdmin, ITabularInline as TabularInline
+from iadmin.options import IModelAdmin, ITabularInline
