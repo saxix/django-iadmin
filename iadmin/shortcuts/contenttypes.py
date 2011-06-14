b4041b5f4ae3ab0f4830b8bba40216ea48bb6abf
saxix <saxix.rome@gmail.com>
2011-06-14 11:10:44 +0200

    improved ContentTypeAdmin

--- iadmin/shortcuts/contenttypes.py
+++ iadmin/shortcuts/contenttypes.py
@@ -2,9 +2,11 @@ from django.contrib.contenttypes.models import ContentType
 from iadmin.options import IModelAdmin
 import iadmin.proxy as admin
 
-class ContentTypeAdmin(IModelAdmin, SA):
-    pass
+class ContentTypeAdmin(IModelAdmin):
+    list_filter = ['app_label',]
+    list_display = list_display_links = ['name', 'app_label', 'model']
+    search_fields = ('name', 'app_label',)
 
 
-admin.site.unregister(ContentType)
+admin.site.silent_unregister(ContentType)
 admin.site.register(ContentType, ContentTypeAdmin)
