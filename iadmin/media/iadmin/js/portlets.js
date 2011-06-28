52f30a1c2ac888fd7db17dbf26f509cb585f800d
saxix <saxix.rome@gmail.com>
2011-06-28 11:24:00 +0200

    added handling expand/collapse of portlets, unique cookie for layout

--- iadmin/media/iadmin/js/portlets.js
+++ iadmin/media/iadmin/js/portlets.js
@@ -1,49 +1,74 @@
-var init_portlets = function($){
-        $(function() {
-            $(".column").sortable({
-                        connectWith: ".column",
-                        stop: function(event, ui) {
-                            $('.column').each(function() {
-                                var column = $(this).attr('id');
-                                 //console.log( column );
-                                var x = new Array();
-                                $('.portlet', this).each(function() {
-                                    var portlet = $(this).attr('id');
-                                    x.push(portlet);
-                                });
-                                 //console.log( x.join() );
-                                $.cookie(column, x);
-                            });
-                        }
-                    });
-
-            $(".portlet").addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
-                    .find(".portlet-header")
-                    .addClass("ui-widget-header ui-corner-all")
-                    .prepend("<span class='ui-icon ui-icon-minusthick'></span>")
-                    .end()
-                    .find(".portlet-content");
-
-            $(".portlet-header .ui-icon").click(function() {
-                $(this).toggleClass("ui-icon-minusthick").toggleClass("ui-icon-plusthick");
-                $(this).parents(".portlet:first").find(".portlet-content").toggle();
+var init_portlets = function($) {
+    $.unparam = function (value) {
+        var params = {}, pieces = value.split('&'), pair, i, l;
+        // Loop through query string pieces and assign params.
+        for (i = 0,l = pieces.length; i < l; i++) {
+            pair = pieces[i].split('=', 2);
+            // Repeated parameters with the same name are overwritten. Parameters
+            // with no value get set to boolean true.
+            params[decodeURIComponent(pair[0])] = (pair.length == 2 ?
+                    decodeURIComponent(pair[1].replace(/\+/g, ' ')) : true);
+        }
+        return params;
+    };
+    var save_layout = function() {
+        $('.column').each(function(index, value) {
+            //var column = $(this).attr('id');
+            var p = new Array();
+            $('.portlet', this).each(function() {
+                p.push($(this).attr('id') + ":" + $(this).find(".portlet-content").is(":visible"));
             });
+            layout[index] = p.join(",");
+        });
+        $.cookie('layout', $.param(layout), { expires: 365, path: '/' });
+    }
 
-            $(".column").disableSelection();
-
-            $(window).load(function() {
-                $('.column').each(function() {
-                    var column = $(this).attr('id');
-                    var ck = $.cookie(column);
-                    if (ck){
-                        var e = ck.split(',');
-                        $.each(e, function(index, value) {
-                            var portlet = $('#' + value);
-                            $(portlet).detach().appendTo('#' + column);
-                        });
+    var restore_layout = function() {
+        var layout = $.unparam($.cookie('layout'));
+        var column = null, portlets = null, params = null;
+        $('.column').each(function(col) {
+            //column = $(this).attr('id');
+            portlets = layout[col].split(",");
+            $.each(portlets, function(index, value) {
+                if (value) {
+                    params = value.split(":");
+                    var portlet = $('#' + params[0]);
+                    $(portlet).detach().appendTo('#column' + (1+col));
+                    if (params[1] == "false") {
+                        $(portlet).find(".portlet-content").hide();
                     }
-                });
+                }
             });
         });
+    }
+
+    var layout = {}
+    $(function() {
+        $(".column").sortable({
+            connectWith: ".column",
+            stop: function(event, ui) {
+                save_layout();
+            }
+        });
+
+        $(".portlet").addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
+                .find(".portlet-header")
+                .addClass("ui-widget-header ui-corner-all")
+                .prepend("<span class='ui-icon ui-icon-minusthick'></span>")
+                .end()
+                .find(".portlet-content");
+
+        $(".portlet-header .ui-icon").click(function() {
+            $(this).toggleClass("ui-icon-minusthick").toggleClass("ui-icon-plusthick");
+            $(this).parents(".portlet:first").find(".portlet-content").toggle();
+            save_layout();
+        });
+
+        $(".column").disableSelection();
+
+        $(window).load(function() {
+            restore_layout();
+        });
+    });
 
 };
