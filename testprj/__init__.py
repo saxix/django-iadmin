0ddedcf824a5bb52246478c6c2fb9ed6cfc490c2
sax <sax@os4d.org>
2011-07-16 23:40:51 +0200

    removed reversion

--- testprj/__init__.py
+++
@@ -1,8 +0,0 @@
-# The next line needs clarification:
-# try to manage the import of django-reversion into `except ImportError` section in django 1.2 cause strange issues,
-# the development server refuse to start or is unable to find the templates once started. Thi works on 1.2 and higher
-import settings
-try:
-    import reversion
-except ImportError:
-    settings.INSTALLED_APPS .remove('reversion')
