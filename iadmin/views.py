857183b75497163b0d7cda37eced422bebccc647
sax <sax@os4d.org>
2011-07-16 23:37:44 +0200

    BACKWARD INCOPATIBLE: removed compatibility with django version prior 1.3 to be ready for 1.4

--- iadmin/views.py
+++ iadmin/views.py
@@ -11,6 +11,7 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.utils.encoding import smart_str
 from .filterspecs import *
+import django
 
 __author__ = 'sax'
 
@@ -32,88 +33,26 @@ class IChangeList(ChangeList):
         super(IChangeList, self).__init__(request, model, list_display, list_display_links, list_filter, date_hierarchy,
                                           search_fields, list_select_related, list_per_page, list_editable, model_admin)
 
+    def active_filters(self):
+        pass
+
     def get_query_set(self, request=None):
-        qs = self.root_query_set
-        lookup_params = self.params.copy() # a dictionary of the query string
-        for i in (ALL_VAR, ORDER_VAR, ORDER_TYPE_VAR, SEARCH_VAR, IS_POPUP_VAR):
-            if i in lookup_params:
-                del lookup_params[i]
+        backup_params = self.params.copy() # a dictionary of the query string
         negate_filters = []
-        for key, value in lookup_params.items():
-            if not isinstance(key, str):
-                # 'key' will be used as a keyword argument later, so Python
-                # requires it to be a string.
-                del lookup_params[key]
-                lookup_params[smart_str(key)] = value
-
-            # if key ends with __in, split parameter into separate values
-            if key.endswith('__in'):
-                lookup_params[key] = value.split(',')
-
+        for key, value in self.params.items():
             if key.endswith('__not'):
-                negate_filters.append(~Q(**{key.replace('__not', ''): value}))
-                del lookup_params[key]
-
-            # if key ends with __isnull, special case '' and false
-            if key.endswith('__isnull'):
-                if value.lower() in ('', 'false'):
-                    lookup_params[key] = False
-                else:
-                    lookup_params[key] = True
-
-        # Apply lookup parameters from the query string.
-        try:
-            qs = qs.filter(**lookup_params).filter(*negate_filters)
-        # Naked except! Because we don't have any other way of validating "params".
-        # They might be invalid if the keyword arguments are incorrect, or if the
-        # values are not in the correct type, so we might get FieldError, ValueError,
-        # ValicationError, or ? from a custom field that raises yet something else
-        # when handed impossible data.
-        except:
-            raise IncorrectLookupParameters
-
-        # Use select_related() if one of the list_display options is a field
-        # with a relationship and the provided queryset doesn't already have
-        # select_related defined.
-        if not qs.query.select_related:
-            if self.list_select_related:
-                qs = qs.select_related()
-            else:
-                for field_name in self.list_display:
-                    try:
-                        f = self.lookup_opts.get_field(field_name)
-                    except models.FieldDoesNotExist:
-                        pass
-                    else:
-                        if isinstance(f.rel, models.ManyToOneRel):
-                            qs = qs.select_related()
-                            break
-
-        # Set ordering.
-        if self.order_field:
-            qs = qs.order_by('%s%s' % ((self.order_type == 'desc' and '-' or ''), self.order_field))
-
-        # Apply keyword searches.
-        def construct_search(field_name):
-            if field_name.startswith('^'):
-                return "%s__istartswith" % field_name[1:]
-            elif field_name.startswith('='):
-                return "%s__iexact" % field_name[1:]
-            elif field_name.startswith('@'):
-                return "%s__search" % field_name[1:]
-            else:
-                return "%s__icontains" % field_name
-
-        if self.search_fields and self.query:
-            for bit in self.query.split():
-                or_queries = [models.Q(**{construct_search(str(field_name)): bit}) for field_name in self.search_fields]
-                qs = qs.filter(reduce(operator.or_, or_queries))
-            for field_name in self.search_fields:
-                if '__' in field_name:
-                    qs = qs.distinct()
-                    break
-
-        return qs
+                field_name = key.replace('__not', '')
+                negate_filters.append(~Q(**{field_name: value}))
+                del self.params[key]
+                
+        # todo remove backward compatibility 
+        if django.VERSION[0:2] == (1,3):
+            ret = super(IChangeList, self).get_query_set()
+        else:
+            ret = super(IChangeList, self).get_query_set(request)
+        self.negate_filters = [ c.children[0].children[0][0] for c in negate_filters]
+        self.params = backup_params
+        return ret.filter(*negate_filters)
 
 
 AGENTS = {lambda x: 'Firefox' in x: ['Firefox', 'iadmin/nojs/firefox.html'],
