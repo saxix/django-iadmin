305d99887624c3daf735719e87f7cf1cdc826582
sax <sax@os4d.org>
2012-01-31 10:17:44 +0700

    requirements check

--- iadmin/models.py
+++ iadmin/models.py
@@ -1,8 +1,14 @@
+from django.conf import settings
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import signals
 from django.db import models
+from django.core.exceptions import ImproperlyConfigured
 
+_requirements = ('django.contrib.auth', 'django.contrib.sessions')
+for req in _requirements:
+    if req not in settings.INSTALLED_APPS:
+        raise ImproperlyConfigured("Put '%s' in your INSTALLED_APPS setting in order to use the iAdmin." % r)
 
 def create_readonly_permission(sender, **kwargs):
     from django.contrib.auth.models import Permission
