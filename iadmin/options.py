e148c383b51ff29eedd1edccc2738ee1d83b9672
sax <s.apostolico@gmail.com>
2012-05-21 11:53:17 +0700

    fixed delete_view

--- iadmin/options.py
+++ iadmin/options.py
@@ -8,6 +8,8 @@ from django.db import transaction
 from django.db.models.fields import AutoField
 from django.db.models.related import RelatedObject
 from django.db.models.sql.constants import LOOKUP_SEP, QUERY_TERMS
+from django.db import models, transaction, router
+from django.contrib.admin.util import unquote, flatten_fieldsets, get_deleted_objects, model_format_dict
 from django.forms.formsets import all_valid
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.template.response import TemplateResponse, SimpleTemplateResponse
@@ -293,6 +295,68 @@ class IModelAdminMixin(object):
             "iadmin/%s" % template,
             template
         ]
+    @csrf_protect_m
+    @transaction.commit_on_success
+    def delete_view(self, request, object_id, extra_context=None):
+        "The 'delete' admin view for this model."
+        opts = self.model._meta
+        app_label = opts.app_label
+
+        obj = self.get_object(request, unquote(object_id))
+
+        if not self.has_delete_permission(request, obj):
+            raise PermissionDenied
+
+        if obj is None:
+            raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {'name': force_unicode(opts.verbose_name), 'key': escape(object_id)})
+
+        using = router.db_for_write(self.model)
+
+        # Populate deleted_objects, a data structure of all related objects that
+        # will also be deleted.
+        (deleted_objects, perms_needed, protected) = get_deleted_objects(
+            [obj], opts, request.user, self.admin_site, using)
+
+        if request.POST: # The user has already confirmed the deletion.
+            if perms_needed:
+                raise PermissionDenied
+            obj_display = force_unicode(obj)
+            self.log_deletion(request, obj, obj_display)
+            self.delete_model(request, obj)
+
+            self.message_user(request, _('The %(name)s "%(obj)s" was deleted successfully.') % {'name': force_unicode(opts.verbose_name), 'obj': force_unicode(obj_display)})
+
+            if not self.has_change_permission(request, None):
+                return HttpResponseRedirect(reverse('%s:index' % self.admin_site.name,
+                    current_app=self.admin_site.name))
+            return HttpResponseRedirect(reverse('%s:%s_%s_changelist' %
+                                                (self.admin_site.name, opts.app_label, opts.module_name),
+                current_app=self.admin_site.name))
+
+        object_name = force_unicode(opts.verbose_name)
+
+        if perms_needed or protected:
+            title = _("Cannot delete %(name)s") % {"name": object_name}
+        else:
+            title = _("Are you sure?")
+
+        context = {
+            "title": title,
+            "object_name": object_name,
+            "object": obj,
+            "deleted_objects": deleted_objects,
+            "perms_lacking": perms_needed,
+            "protected": protected,
+            "opts": opts,
+            "app_label": app_label,
+            }
+        context.update(extra_context or {})
+
+        return TemplateResponse(request, self.delete_confirmation_template or [
+            "iadmin/%s/%s/delete_confirmation.html" % (app_label, opts.object_name.lower()),
+            "iadmin/%s/delete_confirmation.html" % app_label,
+            "iadmin/delete_confirmation.html"
+        ], context, current_app=self.admin_site.name)
 
     @csrf_protect_m
     @transaction.commit_on_success
