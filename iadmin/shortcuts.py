9f09c916e724f11392c2fe40a8582835b04b06bd
sax <s.apostolico@gmail.com>
2012-06-08 12:09:38 +0200

    added shortcuts for guardian

---
+++ iadmin/shortcuts.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.translation import gettext as _
+from guardian.admin import GuardedModelAdmin
+from iadmin.options import IModelAdmin, IModelAdminMixin
+
+class IGuardedModelAdmin(IModelAdminMixin, GuardedModelAdmin):
+    change_form_template =    'iadmin/guardian/model/change_form.html'
+    obj_perms_manage_template ='iadmin/guardian/model/obj_perms_manage.html'
+    obj_perms_manage_user_template ='iadmin/guardian/model/obj_perms_manage_user.html'
+    obj_perms_manage_group_template ='iadmin/guardian/model/obj_perms_manage_group.html'
+
+
+#    def get_urls(self):
+#        return
